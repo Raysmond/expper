bc186ff7dac7278f9a4abc4149fdfe8d90bd91ee
Raysmond <jiankunlei@126.com>
2015-12-07 09:50:46 +0800

    new account activation routes

--- src/main/java/com/expper/web/ExpperController.java
+++ src/main/java/com/expper/web/ExpperController.java
@@ -8,6 +8,7 @@ import com.expper.security.SecurityUtils;
 import com.expper.service.ContentService;
 import com.expper.service.HotPostService;
 import com.expper.service.PostListService;
+import com.expper.service.UserService;
 import com.expper.service.VoteService;
 import com.expper.service.CountingService;
 import com.expper.web.exceptions.PageNotFoundException;
@@ -18,9 +19,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -35,6 +38,9 @@ public class ExpperController {
 
     @Autowired
     private ContentRepository contentRepository;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "", method = GET)
     @Timed
@@ -53,7 +59,7 @@ public class ExpperController {
     }
 
     @RequestMapping(value = "about", method = GET)
-    public String about(Model model) {
+    public String about(Model model, @ModelAttribute("message") String message) {
         Content content = contentService.getPublishedContent("about");
 
         if (content == null) {
@@ -64,6 +70,8 @@ public class ExpperController {
             content = contentRepository.save(content);
         }
 
+        System.out.println(message);
+        model.addAttribute("message", message);
         model.addAttribute("content", content);
         return "about";
     }
@@ -78,5 +86,16 @@ public class ExpperController {
 
         model.addAttribute("content", content);
         return "content/page";
+    }
+
+    @RequestMapping(value = "user/activate", method = GET)
+    public String activateAccount(@RequestParam(value = "key") String key, RedirectAttributes redirectAttrs) {
+        if (null != userService.activateRegistration(key)) {
+            redirectAttrs.addFlashAttribute("message", "您的账号激活成功！");
+            return "redirect:/me";
+        } else {
+            redirectAttrs.addFlashAttribute("message", "您的账号激活失败！");
+            return "redirect:/me";
+        }
     }
 }
--- src/main/webapp/scripts/app/app.constants.js
+++ src/main/webapp/scripts/app/app.constants.js
@@ -4,6 +4,6 @@ angular.module('expperApp')
 
 .constant('ENV', 'dev')
 
-.constant('VERSION', '1.4.5-SNAPSHOT')
+.constant('VERSION', '1.4.6-SNAPSHOT')
 
 ;
